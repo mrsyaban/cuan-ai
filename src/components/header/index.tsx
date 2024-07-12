b01659def66dfb97e488261eaa7d340fe5924616
mrsyaban <mrsyaban99@gmail.com>
2024-07-12 21:53:40 +0700

    feat: add logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import Logo from "../../../public/logo.svg"
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="w-screen h-[72px] absolute top-0 z-10 flex px-32 justify-between item-center">
-      <Link to="/" className="flex w-fit h-full items-center text-white">
-        Logo
-      </Link>
+    <div className="w-screen h-[72px] items-center absolute top-0 z-10 flex px-32 py-12 justify-between">
+      <div className="flex gap-12 items-center text-sm ">
+
+        <img src={Logo} onClick={()=>navigate("/")} className="flex cursor-pointer h-[100px] w-[100px] items-center text-white"/>
+        UNLOCK THE TRUE POTENTIAL OF INVESTMENT
+      </div>
+        
       <Link to="/login" className="cursor-pointer border-2 hover:bg-opacity-40 hover:text-white border-pink-400 bg-pink-300 bg-opacity-25 w-fit self-center h-fit py-1 px-8 rounded-full text-white text-lg">
         Subscribe
       </Link>
